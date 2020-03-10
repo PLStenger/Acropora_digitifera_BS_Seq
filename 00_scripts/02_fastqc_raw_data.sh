5277a2c1b2ccfbf02b45210379f7c6bdc8e65346
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-03-10 17:13:43 +1100

    Create 02_fastqc_raw_data.sh

---
+++ 00_scripts/02_fastqc_raw_data.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+
+#fastqc ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Acclimation_3_30a_R1.fastq -o ~/stenger_data/EPIGENETIC_DATA/02_fastqc_raw
+fastqc ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Acclimation_3_30a_R2.fastq -o ~/stenger_data/EPIGENETIC_DATA/02_fastqc_raw
+fastqc ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Acclimation_3_30b_R1.fastq -o ~/stenger_data/EPIGENETIC_DATA/02_fastqc_raw
+fastqc ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Acclimation_3_30b_R2.fastq -o ~/stenger_data/EPIGENETIC_DATA/02_fastqc_raw
+fastqc ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Acclimation_3_30c_R1.fastq -o ~/stenger_data/EPIGENETIC_DATA/02_fastqc_raw
+fastqc ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Acclimation_3_30c_R2.fastq -o ~/stenger_data/EPIGENETIC_DATA/02_fastqc_raw
+fastqc ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Acclimation_1_31_5a_R1.fastq -o ~/stenger_data/EPIGENETIC_DATA/02_fastqc_raw
+fastqc ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Acclimation_1_31_5a_R2.fastq -o ~/stenger_data/EPIGENETIC_DATA/02_fastqc_raw
+fastqc ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Acclimation_1_31_5b_R1.fastq -o ~/stenger_data/EPIGENETIC_DATA/02_fastqc_raw
+fastqc ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Acclimation_1_31_5b_R2.fastq -o ~/stenger_data/EPIGENETIC_DATA/02_fastqc_raw
+fastqc ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Acclimation_1_31_5c_R1.fastq -o ~/stenger_data/EPIGENETIC_DATA/02_fastqc_raw
+fastqc ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Acclimation_1_31_5c_R2.fastq -o ~/stenger_data/EPIGENETIC_DATA/02_fastqc_raw
+fastqc ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Control_3_30a_R1.fastq -o ~/stenger_data/EPIGENETIC_DATA/02_fastqc_raw
+fastqc ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Control_3_30a_R2.fastq -o ~/stenger_data/EPIGENETIC_DATA/02_fastqc_raw
+fastqc ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Control_3_30b_R1.fastq -o ~/stenger_data/EPIGENETIC_DATA/02_fastqc_raw
+fastqc ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Control_3_30b_R2.fastq -o ~/stenger_data/EPIGENETIC_DATA/02_fastqc_raw
+fastqc ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Control_3_30c_R1.fastq -o ~/stenger_data/EPIGENETIC_DATA/02_fastqc_raw
+fastqc ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Control_3_30c_R2.fastq -o ~/stenger_data/EPIGENETIC_DATA/02_fastqc_raw
+fastqc ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Control_1_30c_R1.fastq -o ~/stenger_data/EPIGENETIC_DATA/02_fastqc_raw
+fastqc ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Control_1_30c_R2.fastq -o ~/stenger_data/EPIGENETIC_DATA/02_fastqc_raw
+fastqc ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Control_1_30a_R1.fastq -o ~/stenger_data/EPIGENETIC_DATA/02_fastqc_raw
+fastqc ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Control_1_30a_R2.fastq -o ~/stenger_data/EPIGENETIC_DATA/02_fastqc_raw
+fastqc ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Control_1_30b_R1.fastq -o ~/stenger_data/EPIGENETIC_DATA/02_fastqc_raw
+fastqc ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Control_1_30b_R2.fastq -o ~/stenger_data/EPIGENETIC_DATA/02_fastqc_raw
+
+
