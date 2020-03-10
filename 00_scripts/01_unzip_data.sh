38d79fdc3a61c95cc33b57350c3ab5fab4423c6a
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-03-10 17:11:19 +1100

    Create 01_unzip_data.sh

---
+++ 00_scripts/01_unzip_data.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+
+zcat ~/stenger_data/EPIGENETIC_DATA/RAW_DATA/aA10_CTAGCT.R1C.fast.gz > ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Acclimation_3_30a_R1.fastq
+zcat ~/stenger_data/EPIGENETIC_DATA/RAW_DATA/aA10_CTAGCT.R2C.fast.gz > ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Acclimation_3_30a_R2.fastq
+zcat ~/stenger_data/EPIGENETIC_DATA/RAW_DATA/aA12_TATAAT.R1C.fast.gz > ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Acclimation_3_30b_R1.fastq
+zcat ~/stenger_data/EPIGENETIC_DATA/RAW_DATA/aA12_TATAAT.R2C.fast.gz > ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Acclimation_3_30b_R2.fastq
+zcat ~/stenger_data/EPIGENETIC_DATA/RAW_DATA/aA7_GCGCTA.R1C.fast.gz > ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Acclimation_3_30c_R1.fastq
+zcat ~/stenger_data/EPIGENETIC_DATA/RAW_DATA/aA7_GCGCTA.R2C.fast.gz > ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Acclimation_3_30c_R2.fastq
+zcat ~/stenger_data/EPIGENETIC_DATA/RAW_DATA/aC11_CACGAT.R1C.fast.gz > ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Acclimation_1_31_5a_R1.fastq
+zcat ~/stenger_data/EPIGENETIC_DATA/RAW_DATA/aC11_CACGAT.R2C.fast.gz > ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Acclimation_1_31_5a_R2.fastq
+zcat ~/stenger_data/EPIGENETIC_DATA/RAW_DATA/aC3_CAACTA.R1C.fast.gz > ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Acclimation_1_31_5b_R1.fastq
+zcat ~/stenger_data/EPIGENETIC_DATA/RAW_DATA/aC3_CAACTA.R2C.fast.gz > ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Acclimation_1_31_5b_R2.fastq
+zcat ~/stenger_data/EPIGENETIC_DATA/RAW_DATA/aCX1_CACCGG.R1C.fast.gz > ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Acclimation_1_31_5c_R1.fastq
+zcat ~/stenger_data/EPIGENETIC_DATA/RAW_DATA/aCX1_CACCGG.R2C.fast.gz > ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Acclimation_1_31_5c_R2.fastq
+zcat ~/stenger_data/EPIGENETIC_DATA/RAW_DATA/cA1_CATGGC.R1C.fast.gz > ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Control_3_30a_R1.fastq
+zcat ~/stenger_data/EPIGENETIC_DATA/RAW_DATA/cA1_CATGGC.R2C.fast.gz > ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Control_3_30a_R2.fastq
+zcat ~/stenger_data/EPIGENETIC_DATA/RAW_DATA/cA4_CACTCA.R1C.fast.gz > ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Control_3_30b_R1.fastq
+zcat ~/stenger_data/EPIGENETIC_DATA/RAW_DATA/cA4_CACTCA.R2C.fast.gz > ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Control_3_30b_R2.fastq
+zcat ~/stenger_data/EPIGENETIC_DATA/RAW_DATA/cA9_CAGGCG.R1C.fast.gz > ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Control_3_30c_R1.fastq
+zcat ~/stenger_data/EPIGENETIC_DATA/RAW_DATA/cA9_CAGGCG.R2C.fast.gz > ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Control_3_30c_R2.fastq
+zcat ~/stenger_data/EPIGENETIC_DATA/RAW_DATA/cC10_CAAAAG.R1C.fast.gz > ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Control_1_30c_R1.fastq
+zcat ~/stenger_data/EPIGENETIC_DATA/RAW_DATA/cC10_CAAAAG.R2C.fast.gz > ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Control_1_30c_R2.fastq
+zcat ~/stenger_data/EPIGENETIC_DATA/RAW_DATA/cC2_ATGAGC.R1C.fast.gz > ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Control_1_30a_R1.fastq
+zcat ~/stenger_data/EPIGENETIC_DATA/RAW_DATA/cC2_ATGAGC.R2C.fast.gz > ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Control_1_30a_R2.fastq
+zcat ~/stenger_data/EPIGENETIC_DATA/RAW_DATA/cC5_GGTAGC.R1C.fast.gz > ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Control_1_30b_R1.fastq
+zcat ~/stenger_data/EPIGENETIC_DATA/RAW_DATA/cC5_GGTAGC.R2C.fast.gz > ~/stenger_data/EPIGENETIC_DATA/01_data_unzipped/Control_1_30b_R2.fastq
+
