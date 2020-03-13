6724337bda62660cf1a2d0fe01eef0f1565fcba1
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-03-14 09:16:25 +1100

    Create 05_bismark_genome_preparation.sh

---
+++ 00_scripts/05_bismark_genome_preparation.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+bismark_genome_preparation /home/stenger/stenger_data/NEW_GENOME_ADIG2020/GCF_000222465.1_Adig_1.1_genomic_fna/GCF_000222465.1_Adig_1.1_genomic_fna/GCF_000222465.1_Adig_1.1_genomic.fna
+
+# If not working lunch:
+# bismark_genome_preparation /home/stenger/stenger_data/NEW_GENOME_ADIG2020/GCF_000222465.1_Adig_1.1_genomic_fna/GCF_000222465.1_Adig_1.1_genomic_fna
