e7b089c9638b9045304f10fa82edc37ea975c894
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-03-14 18:50:54 +1100

    Update 06_bismark_alignment.sh

--- 00_scripts/06_bismark_alignment.sh
+++ 00_scripts/06_bismark_alignment.sh
@@ -11,6 +11,8 @@
   # E.g. if -X 100 is specified and a paired-end alignment consists of two 20-bp alignments in the proper orientation with a 60-bp gap between them, that alignment is considered valid (as long as -I is also satisfied). A 61-bp gap would not be valid in that case. Default: 500. Alignment option -X/--maxins <INT>
 # -p 4 =			#THREADS
 
+# -n needs to be between 0 and 1
+
 
 GENOME=/home/stenger/stenger_data/NEW_GENOME_ADIG2020/GCF_000222465.1_Adig_1.1_genomic_fna/GCF_000222465.1_Adig_1.1_genomic_fna
 READS=/home/stenger/stenger_data/EPIGENETIC_DATA/03_trimmed
@@ -22,6 +24,6 @@ cd $OUTPUT
 #bismark -l 20 -n 0 -p 4 -q --genome $GENOME -1 $READS/Acclimation_1_31_5a_R1_paired.fastq.gz -2 $READS/Acclimation_1_31_5a_R2_paired.fastq.gz > $OUTPUT/Acclimation_1_31_5a.sam
 #bismark -l 10 -n 0 -p 4 -q --genome $GENOME -1 $READS/Acclimation_1_31_5a_R1_paired.fastq.gz -2 $READS/Acclimation_1_31_5a_R2_paired.fastq.gz > $OUTPUT/Acclimation_1_31_5a.sam
 
-bismark -l 20 -n 5 -p 4 -q --genome $GENOME -1 $READS/Acclimation_1_31_5a_R1_paired.fastq.gz -2 $READS/Acclimation_1_31_5a_R2_paired.fastq.gz > $OUTPUT/Acclimation_1_31_5a.sam
+bismark -l 20 -n 1 -p 4 -q --genome $GENOME -1 $READS/Acclimation_1_31_5a_R1_paired.fastq.gz -2 $READS/Acclimation_1_31_5a_R2_paired.fastq.gz > $OUTPUT/Acclimation_1_31_5a.sam
 
 
