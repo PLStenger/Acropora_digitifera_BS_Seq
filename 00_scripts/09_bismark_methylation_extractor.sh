44d7ed0e41241841f939df527b113e97033d8599
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-03-16 11:36:47 +1100

    Create 09_bismark_methylation_extractor.sh

---
+++ 00_scripts/09_bismark_methylation_extractor.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+DATADIRECTORY=/home/stenger/stenger_data/EPIGENETIC_DATA/05_bismark
+GENOME=/home/stenger/stenger_data/NEW_GENOME_ADIG2020/GCF_000222465.1_Adig_1.1_genomic_fna/GCF_000222465.1_Adig_1.1_genomic_fna
+NCPU=4
+
+cd $DATADIRECTORY
+
+for FILE in $(ls $DATADIRECTORY/*_sorted.bam)
+
+do
+
+bismark_methylation_extractor -p --no_overlap --bedGraph --scaffolds --cytosine_report --genome_folder $GENOME --multicore $NCPU $DATADIRECTORY/${FILE##*/} -o $DATADIRECTORY
+
+done ;
