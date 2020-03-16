5d1ba73adf06618d9f6146bc748d3706c677677b
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-03-16 17:41:21 +1100

    Update 04_fastqc_trimmed.sh

--- 00_scripts/04_fastqc_trimmed.sh
+++ 00_scripts/04_fastqc_trimmed.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
-fastqc /home/stenger/stenger_data/EPIGENETIC_DATA/03_trimmed/Acclimation_1_31_5a_R1_paired.fastq.gz -o ~/stenger_data/EPIGENETIC_DATA/04_fastqc_trimmed
+DATADIRECTORY=/home/stenger/stenger_data/EPIGENETIC_DATA/03_trimmed
+OUTPUT=/home/stenger/stenger_data/EPIGENETIC_DATA/04_fastqc_trimmed
+
+cd $DATADIRECTORY
+
+for FILE in $(ls $DATADIRECTORY/*fastq.gz)
+
+do
+
+fastqc ${FILE##*/} -o $OUTPUT
+
+done;
+
+#fastqc /home/stenger/stenger_data/EPIGENETIC_DATA/03_trimmed/Acclimation_1_31_5a_R1_paired.fastq.gz -o ~/stenger_data/EPIGENETIC_DATA/04_fastqc_trimmed
 
 
