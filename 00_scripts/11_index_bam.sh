8d95eef3a51650ec7f3a77ae5a797e60e0de8662
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-03-17 11:11:13 +1100

    Create 11_index_bam.sh

---
+++ 00_scripts/11_index_bam.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+DATADIRECTORY=/home/stenger/stenger_data/EPIGENETIC_DATA/05_bismark
+
+cd $DATADIRECTORY
+
+for FILE in $(ls $DATADIRECTORY/*_sorted.bam)
+
+do
+samtools index ${FILE##*/} ${FILE##*/}.bai
+done;
+
