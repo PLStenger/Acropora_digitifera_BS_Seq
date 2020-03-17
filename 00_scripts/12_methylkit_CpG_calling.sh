246209357f9df8d492da3bcaa8735969c35e2e02
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-03-17 11:19:45 +1100

    Create 12_methylkit_CpG_calling.sh

---
+++ 00_scripts/12_methylkit_CpG_calling.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+DATADIRECTORY=/home/stenger/stenger_data/EPIGENETIC_DATA/05_bismark
+OUTPUT=/home/stenger/stenger_data/EPIGENETIC_DATA/07_methylkit
+
+SCRIPT_FOLDER=/home/stenger/stenger_data/EPIGENETIC_DATA/Acropora_digitifera_BS_Seq/00_scripts
+
+cd $DATADIRECTORY
+
+
+SCRIPT_R=/home/stenger/stenger_data/EPIGENETIC_DATA/Acropora_digitifera_BS_Seq/00_scripts/12_methylkit_CpG_calling.R
+
+Rscript --vanilla $SCRIPT_R > ${SCRIPT_FOLDER}/12_methylkit_CpG_calling.out
+
