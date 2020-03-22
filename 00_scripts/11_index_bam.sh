07379da862d5a92ddf9c7a6b5e74e34bae752110
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-03-22 11:21:15 +1100

    Update 11_index_bam.sh

--- 00_scripts/11_index_bam.sh
+++ 00_scripts/11_index_bam.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DATADIRECTORY=/home/stenger/stenger_data/EPIGENETIC_DATA/05_bismark
+DATADIRECTORY=/home/stenger/stenger_data/EPIGENETIC_DATA/05_02_bismark
 
 cd $DATADIRECTORY
 
