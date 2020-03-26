06528dfd0627e5b760e37f5b01ecbcbb8c5c76b6
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-03-27 09:55:57 +1100

    Update 12_methylkit_CpG_calling.R

--- 00_scripts/12_methylkit_CpG_calling.R
+++ 00_scripts/12_methylkit_CpG_calling.R
@@ -6,116 +6,118 @@ library(tools)
 
 setwd("/home/stenger/stenger_data/EPIGENETIC_DATA/05_02_bismark")
 
-#########################################################################################
-### GET METHYLATION FROM SORTED BISMARK ALIGNEMENT OR READ THE METHYLATION CALL FILES ###
-#########################################################################################
+##########################################################################################
+#### GET METHYLATION FROM SORTED BISMARK ALIGNEMENT OR READ THE METHYLATION CALL FILES ###
+##########################################################################################
+#
+####### ALL
+#list.bam_acclimation=list("Acclimation_1_31_5a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam",
+#                          "Acclimation_1_31_5b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam",
+#                          "Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam",
+#			  "Acclimation_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam",
+#		          "Acclimation_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam",
+#                          "Acclimation_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam",
+#                          "Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam",
+#		          "Control_1_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam",
+#		          "Control_1_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam",
+#	                  "Control_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam",
+#	                  "Control_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam",
+#		          "Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam")
+#
+#list.id_acclimation=list("Acclimation_1_31_5a",
+#                         "Acclimation_1_31_5b",
+#                         "Acclimation_1_31_5c",
+#                         "Acclimation_3_30a",
+#                         "Acclimation_3_30b",
+#                         "Acclimation_3_30c",
+#                         "Control_1_30a",
+#                         "Control_1_30b",
+#                         "Control_1_30c",
+#                         "Control_3_30a",
+#                         "Control_3_30b",
+#                         "Control_3_30c")
+#
+###############################################################################################################
+####################################################### CPG ###################################################
+###############################################################################################################
+#
+#my_meth_acclimation=processBismarkAln(location=list.bam_acclimation,
+#                sample.id=list.id_acclimation,
+#                assembly="GCF_000222465.1_Adig_1.1_genomic_database_06_col1.fasta",
+#                save.folder="methylation_call_acclimation",
+#                save.context=c("CpG"),
+#                read.context="CpG",
+#                mincov=10,
+#                treatment=c(0,0,0,1,1,1,2,2,2,3,3,3))
+#
+## Descriptive statistics on samples
+#print("my_meth_acclimation[[1]]")
+#my_meth_acclimation[[1]]
+#print("my_meth_acclimation[[2]]")
+#my_meth_acclimation[[2]]
+#print("my_meth_acclimation[[3]]")
+#my_meth_acclimation[[3]]
+#print("my_meth_acclimation[[4]]")
+#my_meth_acclimation[[4]]
+#print("my_meth_acclimation[[5]]")
+#my_meth_acclimation[[5]]
+#print("my_meth_acclimation[[6]]")
+#my_meth_acclimation[[6]]
+#print("my_meth_acclimation[[7]]")
+#my_meth_acclimation[[7]]
+#print("my_meth_acclimation[[8]]")
+#my_meth_acclimation[[8]]
+#print("my_meth_acclimation[[9]]")
+#my_meth_acclimation[[9]]
+#print("my_meth_acclimation[[10]]")
+#my_meth_acclimation[[10]]
+#print("my_meth_acclimation[[11]]")
+#my_meth_acclimation[[11]]
+#print("my_meth_acclimation[[12]]")
+#my_meth_acclimation[[12]]
+#getMethylationStats(my_meth_acclimation[[1]],plot=FALSE,both.strands=FALSE)
+#getMethylationStats(my_meth_acclimation[[1]],plot=TRUE,both.strands=FALSE)
+#getCoverageStats(my_meth_acclimation[[1]],plot=TRUE,both.strands=FALSE)
+#getMethylationStats(my_meth_acclimation[[2]],plot=FALSE,both.strands=FALSE)
+#getMethylationStats(my_meth_acclimation[[2]],plot=TRUE,both.strands=FALSE)
+#getCoverageStats(my_meth_acclimation[[2]],plot=TRUE,both.strands=FALSE)
+#getMethylationStats(my_meth_acclimation[[3]],plot=FALSE,both.strands=FALSE)
+#getMethylationStats(my_meth_acclimation[[3]],plot=TRUE,both.strands=FALSE)
+#getCoverageStats(my_meth_acclimation[[3]],plot=TRUE,both.strands=FALSE)
+#getMethylationStats(my_meth_acclimation[[4]],plot=FALSE,both.strands=FALSE)
+#getMethylationStats(my_meth_acclimation[[4]],plot=TRUE,both.strands=FALSE)
+#getCoverageStats(my_meth_acclimation[[4]],plot=TRUE,both.strands=FALSE)
+#getMethylationStats(my_meth_acclimation[[5]],plot=FALSE,both.strands=FALSE)
+#getMethylationStats(my_meth_acclimation[[5]],plot=TRUE,both.strands=FALSE)
+#getCoverageStats(my_meth_acclimation[[5]],plot=TRUE,both.strands=FALSE)
+#getMethylationStats(my_meth_acclimation[[6]],plot=FALSE,both.strands=FALSE)
+#getMethylationStats(my_meth_acclimation[[6]],plot=TRUE,both.strands=FALSE)
+#getCoverageStats(my_meth_acclimation[[6]],plot=TRUE,both.strands=FALSE)
+#getMethylationStats(my_meth_acclimation[[7]],plot=FALSE,both.strands=FALSE)
+#getMethylationStats(my_meth_acclimation[[7]],plot=TRUE,both.strands=FALSE)
+#getCoverageStats(my_meth_acclimation[[7]],plot=TRUE,both.strands=FALSE)
+#getMethylationStats(my_meth_acclimation[[8]],plot=FALSE,both.strands=FALSE)
+#getMethylationStats(my_meth_acclimation[[8]],plot=TRUE,both.strands=FALSE)
+#getCoverageStats(my_meth_acclimation[[8]],plot=TRUE,both.strands=FALSE)
+#getMethylationStats(my_meth_acclimation[[9]],plot=FALSE,both.strands=FALSE)
+#getMethylationStats(my_meth_acclimation[[9]],plot=TRUE,both.strands=FALSE)
+#getCoverageStats(my_meth_acclimation[[9]],plot=TRUE,both.strands=FALSE)
+#getMethylationStats(my_meth_acclimation[[10]],plot=FALSE,both.strands=FALSE)
+#getMethylationStats(my_meth_acclimation[[10]],plot=TRUE,both.strands=FALSE)
+#getCoverageStats(my_meth_acclimation[[10]],plot=TRUE,both.strands=FALSE)
+#getMethylationStats(my_meth_acclimation[[11]],plot=FALSE,both.strands=FALSE)
+#getMethylationStats(my_meth_acclimation[[11]],plot=TRUE,both.strands=FALSE)
+#getCoverageStats(my_meth_acclimation[[11]],plot=TRUE,both.strands=FALSE)
+#getMethylationStats(my_meth_acclimation[[12]],plot=FALSE,both.strands=FALSE)
+#getMethylationStats(my_meth_acclimation[[12]],plot=TRUE,both.strands=FALSE)
+#getCoverageStats(my_meth_acclimation[[12]],plot=TRUE,both.strands=FALSE)
+#
+#save(my_meth_acclimation, file = "my_meth_acclimation.rda")
+##print(my_meth_acclimation[[1]]@dbpath)
+#message("my_meth_acclimation.done")
 
-###### ALL
-list.bam_acclimation=list("Acclimation_1_31_5a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam",
-                          "Acclimation_1_31_5b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam",
-                          "Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam",
-			  "Acclimation_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam",
-		          "Acclimation_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam",
-                          "Acclimation_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam",
-                          "Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam",
-		          "Control_1_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam",
-		          "Control_1_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam",
-	                  "Control_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam",
-	                  "Control_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam",
-		          "Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam")
-
-list.id_acclimation=list("Acclimation_1_31_5a",
-                         "Acclimation_1_31_5b",
-                         "Acclimation_1_31_5c",
-                         "Acclimation_3_30a",
-                         "Acclimation_3_30b",
-                         "Acclimation_3_30c",
-                         "Control_1_30a",
-                         "Control_1_30b",
-                         "Control_1_30c",
-                         "Control_3_30a",
-                         "Control_3_30b",
-                         "Control_3_30c")
-
-##############################################################################################################
-###################################################### CPG ###################################################
-##############################################################################################################
-
-my_meth_acclimation=processBismarkAln(location=list.bam_acclimation,
-                sample.id=list.id_acclimation,
-                assembly="GCF_000222465.1_Adig_1.1_genomic_database_06_col1.fasta",
-                save.folder="methylation_call_acclimation",
-                save.context=c("CpG"),
-                read.context="CpG",
-                mincov=10,
-                treatment=c(0,0,0,1,1,1,2,2,2,3,3,3))
-
-# Descriptive statistics on samples
-print("my_meth_acclimation[[1]]")
-my_meth_acclimation[[1]]
-print("my_meth_acclimation[[2]]")
-my_meth_acclimation[[2]]
-print("my_meth_acclimation[[3]]")
-my_meth_acclimation[[3]]
-print("my_meth_acclimation[[4]]")
-my_meth_acclimation[[4]]
-print("my_meth_acclimation[[5]]")
-my_meth_acclimation[[5]]
-print("my_meth_acclimation[[6]]")
-my_meth_acclimation[[6]]
-print("my_meth_acclimation[[7]]")
-my_meth_acclimation[[7]]
-print("my_meth_acclimation[[8]]")
-my_meth_acclimation[[8]]
-print("my_meth_acclimation[[9]]")
-my_meth_acclimation[[9]]
-print("my_meth_acclimation[[10]]")
-my_meth_acclimation[[10]]
-print("my_meth_acclimation[[11]]")
-my_meth_acclimation[[11]]
-print("my_meth_acclimation[[12]]")
-my_meth_acclimation[[12]]
-getMethylationStats(my_meth_acclimation[[1]],plot=FALSE,both.strands=FALSE)
-getMethylationStats(my_meth_acclimation[[1]],plot=TRUE,both.strands=FALSE)
-getCoverageStats(my_meth_acclimation[[1]],plot=TRUE,both.strands=FALSE)
-getMethylationStats(my_meth_acclimation[[2]],plot=FALSE,both.strands=FALSE)
-getMethylationStats(my_meth_acclimation[[2]],plot=TRUE,both.strands=FALSE)
-getCoverageStats(my_meth_acclimation[[2]],plot=TRUE,both.strands=FALSE)
-getMethylationStats(my_meth_acclimation[[3]],plot=FALSE,both.strands=FALSE)
-getMethylationStats(my_meth_acclimation[[3]],plot=TRUE,both.strands=FALSE)
-getCoverageStats(my_meth_acclimation[[3]],plot=TRUE,both.strands=FALSE)
-getMethylationStats(my_meth_acclimation[[4]],plot=FALSE,both.strands=FALSE)
-getMethylationStats(my_meth_acclimation[[4]],plot=TRUE,both.strands=FALSE)
-getCoverageStats(my_meth_acclimation[[4]],plot=TRUE,both.strands=FALSE)
-getMethylationStats(my_meth_acclimation[[5]],plot=FALSE,both.strands=FALSE)
-getMethylationStats(my_meth_acclimation[[5]],plot=TRUE,both.strands=FALSE)
-getCoverageStats(my_meth_acclimation[[5]],plot=TRUE,both.strands=FALSE)
-getMethylationStats(my_meth_acclimation[[6]],plot=FALSE,both.strands=FALSE)
-getMethylationStats(my_meth_acclimation[[6]],plot=TRUE,both.strands=FALSE)
-getCoverageStats(my_meth_acclimation[[6]],plot=TRUE,both.strands=FALSE)
-getMethylationStats(my_meth_acclimation[[7]],plot=FALSE,both.strands=FALSE)
-getMethylationStats(my_meth_acclimation[[7]],plot=TRUE,both.strands=FALSE)
-getCoverageStats(my_meth_acclimation[[7]],plot=TRUE,both.strands=FALSE)
-getMethylationStats(my_meth_acclimation[[8]],plot=FALSE,both.strands=FALSE)
-getMethylationStats(my_meth_acclimation[[8]],plot=TRUE,both.strands=FALSE)
-getCoverageStats(my_meth_acclimation[[8]],plot=TRUE,both.strands=FALSE)
-getMethylationStats(my_meth_acclimation[[9]],plot=FALSE,both.strands=FALSE)
-getMethylationStats(my_meth_acclimation[[9]],plot=TRUE,both.strands=FALSE)
-getCoverageStats(my_meth_acclimation[[9]],plot=TRUE,both.strands=FALSE)
-getMethylationStats(my_meth_acclimation[[10]],plot=FALSE,both.strands=FALSE)
-getMethylationStats(my_meth_acclimation[[10]],plot=TRUE,both.strands=FALSE)
-getCoverageStats(my_meth_acclimation[[10]],plot=TRUE,both.strands=FALSE)
-getMethylationStats(my_meth_acclimation[[11]],plot=FALSE,both.strands=FALSE)
-getMethylationStats(my_meth_acclimation[[11]],plot=TRUE,both.strands=FALSE)
-getCoverageStats(my_meth_acclimation[[11]],plot=TRUE,both.strands=FALSE)
-getMethylationStats(my_meth_acclimation[[12]],plot=FALSE,both.strands=FALSE)
-getMethylationStats(my_meth_acclimation[[12]],plot=TRUE,both.strands=FALSE)
-getCoverageStats(my_meth_acclimation[[12]],plot=TRUE,both.strands=FALSE)
-
-save(my_meth_acclimation, file = "my_meth_acclimation.rda")
-#print(my_meth_acclimation[[1]]@dbpath)
-message("my_meth_acclimation.done")
-
+load("my_meth_acclimation.rda")
+message("load(my_meth_acclimation.rda).done")
 
 # with normalization #
 normalized.myallmeth=normalizeCoverage(my_meth_acclimation)
@@ -128,7 +130,7 @@ filtered.normalized.myallmeth.10=filterByCoverage(normalized.myallmeth,
 		hi.count = NULL,
 		hi.perc = 99.9)
 
-save(filtered.normalized.myallmeth.10, file = "R.objects/filtered.normalized.myallmeth.10.rda")
+save(filtered.normalized.myallmeth.10, file = "filtered.normalized.myallmeth.10.rda")
 message("filtered.normalized.myallmeth.done")
 
 ### COMPARATIVE ANALYSIS ###
@@ -136,13 +138,11 @@ message("filtered.normalized.myallmeth.done")
 ## Per Base ##
 
 # with normalization #
-load("R.objects/filtered.normalized.myallmeth.10.rda")
-Allmeth.norm=unite(filtered.normalized.myallmeth.10,destrand = FALSE)
+load("filtered.normalized.myallmeth.10.rda")
+Allmeth.norm=unite(filtered.normalized.myallmeth.10, destrand = FALSE)
 
-save(Allmeth.norm, file = "R.objects/Allmeth.norm.rda")
+save(Allmeth.norm, file = "Allmeth.norm.rda")
 message("unite.filtered.normalized.myallmeth.done")
-
-
 
 ##### PCA, infos, and hclust
 layout(matrix(c(1,1,2,3), 2, 2, byrow = TRUE))
