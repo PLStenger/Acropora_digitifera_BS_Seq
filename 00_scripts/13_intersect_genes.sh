ea0c606b8ce01b0f123cefaff735121dd9e58352
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-03-30 20:01:03 +1100

    Update 13_intersect_genes.sh

--- 00_scripts/13_intersect_genes.sh
+++ 00_scripts/13_intersect_genes.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
 
 DATADIRECTORY=/home/stenger/stenger_data/EPIGENETIC_DATA/05_02_bismark
-DATABASE=/home/stenger/stenger_data/NEW_GENOME_ADIG2020/00_custom/Database_ID_gene_from_BS_unique.txt
+DATABASE=/home/stenger/stenger_data/NEW_GENOME_ADIG2020/00_custom/Database_ID_gene_from_BS_unique_02.txt
 
-# Creation database "unique" by R:
-# Database_ID_gene_from_BS <- read.table("Database_ID_gene_from_BS.txt", header=T)
-# head(Database_ID_gene_from_BS)
-# for (i in 1:length(unique(Database_ID_gene_from_BS$gene_id))){
-#  sink("Database_ID_gene_from_BS_unique.txt", append=TRUE)
-#  print(c(as.character(unique(Database_ID_gene_from_BS$scaffold)[i]), 
-#          min(unique(Database_ID_gene_from_BS$start)[i]), 
-#          max(unique(Database_ID_gene_from_BS$stop)[i]),
-#          as.character(unique(Database_ID_gene_from_BS$gene_id)[i])))
-#  sink()
+#Database_ID_gene_from_BS <- read.table("Database_ID_gene_from_BS_02.txt", header=T)
+#head(Database_ID_gene_from_BS)
+#str(Database_ID_gene_from_BS)
+#Database_ID_gene_from_BS$gene_id <- as.character(Database_ID_gene_from_BS$gene_id)
+#Database_ID_gene_from_BS$scaffold <- as.character(Database_ID_gene_from_BS$scaffold)
+#Database_ID_gene_from_BS$start <- as.numeric(Database_ID_gene_from_BS$start)
+#Database_ID_gene_from_BS$stop <- as.numeric(Database_ID_gene_from_BS$stop)
+#str(Database_ID_gene_from_BS)
+
+#for (i in 1:length(unique(Database_ID_gene_from_BS$gene_id))){
+#  sink("Database_ID_gene_from_BS_unique_02.txt", append=TRUE)
+#  
+#  temp <- filter(Database_ID_gene_from_BS, Database_ID_gene_from_BS$gene_id  == unique(Database_ID_gene_from_BS$gene_id)[i])
+#  gene_id <- as.character(unique(temp$gene_id))
+#  start <- min((temp$start))
+#  stop <- max((temp$stop))
+#  scaffold <- as.character(unique(temp$scaffold))
+#  print(c(as.character(gene_id), as.numeric(start), as.numeric(stop), as.character(scaffold)))
+#   sink()
 #}
 
 
