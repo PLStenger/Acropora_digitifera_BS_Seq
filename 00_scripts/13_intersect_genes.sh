d2244b6e7ead9ed4378f9d41a226d75a6246025c
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-03-30 14:49:54 +1100

    Update 13_intersect_genes.sh

--- 00_scripts/13_intersect_genes.sh
+++ 00_scripts/13_intersect_genes.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 DATADIRECTORY=/home/stenger/stenger_data/EPIGENETIC_DATA/05_02_bismark
+DATABASE=/home/stenger/stenger_data/NEW_GENOME_ADIG2020/00_custom/Database_ID_gene_from_BS_unique.txt
 
 cd $DATADIRECTORY
 
@@ -10,7 +11,7 @@ do
 
 awk '{print $2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8}' ${FILE##*/} > ${FILE##*/}_work_in_progress_01.txt
 sed -i '1d' ${FILE##*/}_work_in_progress_01.txt > ${FILE##*/}_work_in_progress_02.txt
-bedtools intersect -a test_data.base.txt -b ${FILE##*/}_work_in_progress_02.txt > ${FILE##*/}_work_in_progress_03.txt
+bedtools intersect -a $DATABASE -b ${FILE##*/}_work_in_progress_02.txt > ${FILE##*/}_work_in_progress_03.txt
 paste ${FILE##*/}_work_in_progress_03.txt ${FILE##*/}_work_in_progress_02.txt > ${FILE##*/}_intersect_genes.txt
 
 done;
