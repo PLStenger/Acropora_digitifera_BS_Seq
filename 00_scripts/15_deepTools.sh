c53239197afc3207c7360f2a8d2bdafb866a9b36
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-04-09 17:53:50 +1100

    Create 15_deepTools.sh

---
+++ 00_scripts/15_deepTools.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+DATADIRECTORY=/home/stenger/stenger_data/EPIGENETIC_DATA/05_02_bismark
+
+cd $DATADIRECTORY
+
+bamCoverage -b Acclimation_1_31_5a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam -o Acclimation_1_31_5a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw
+bamCoverage -b Acclimation_1_31_5b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam -o Acclimation_1_31_5b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw
+bamCoverage -b Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam -o Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw
+bamCoverage -b Acclimation_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam -o Acclimation_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw
+bamCoverage -b Acclimation_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam -o Acclimation_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw
+bamCoverage -b Acclimation_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam -o Acclimation_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw
+bamCoverage -b Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam -o Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw
+bamCoverage -b Control_1_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam -o Control_1_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw
+bamCoverage -b Control_1_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam -o Control_1_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw
+bamCoverage -b Control_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam -o Control_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw
+bamCoverage -b Control_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam -o Control_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw
+bamCoverage -b Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam -o Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw
+
+
+
