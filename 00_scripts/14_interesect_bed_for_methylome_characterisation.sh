ee03bb7ead9b7d37ea79f7c1a27cf630a5202ff1
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-04-09 14:53:03 +1100

    Update 14_interesect_bed_for_methylome_characterisation.sh

--- 00_scripts/14_interesect_bed_for_methylome_characterisation.sh
+++ 00_scripts/14_interesect_bed_for_methylome_characterisation.sh
@@ -2,13 +2,13 @@
 
 DATADIRECTORY=/home/stenger/stenger_data/EPIGENETIC_DATA/06_02_bismark_extractor
 
-for FILE in $(ls $DATADIRECTORY/*bedGraph)
+for FILE in $(ls $DATADIRECTORY/*paired_bismark_bt2_pe.deduplicated.bed)
 
 do
 
-awk '{print $1"\t"$2"\t"$3"\t"$4}' ${FILE##*/} > ${FILE##*/}.bed
-sed -i '1d' ${FILE##*/}.bed
-bedtools intersect -a ${FILE##*/}.bed -b corespondance.bed -wa -wb > ${FILE##*/}_correspondance.txt
+#awk '{print $1"\t"$2"\t"$3"\t"$4}' ${FILE##*/} > ${FILE##*/}.bed
+#sed -i '1d' ${FILE##*/}.bed
+bedtools intersect -a ${FILE##*/} -b corespondance.bed -wa -wb > ${FILE##*/}_correspondance.txt
 
 done;
 
