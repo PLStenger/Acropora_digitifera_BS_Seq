00d3d550bb78dcb684b5782fa275072e53be58ec
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-04-13 10:20:53 +1100

    Update 16_08_compute_matrix.sh

--- 00_scripts/16_08_compute_matrix.sh
+++ 00_scripts/16_08_compute_matrix.sh
@@ -6,5 +6,6 @@ cd $DATADIRECTORY
 
 computeMatrix scale-regions -S Control_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R scaffold_gene_lenght_02.bed  --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o Control_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz
 
-plotProfile -m Control_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz -out Control_3_30bControl_3_30b.pdf --plotTitle "Control_3_30b" --plotHeight 10 --plotWidth 10
+#plotProfile -m Control_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz -out Control_3_30bControl_3_30b.pdf --plotTitle "Control_3_30b" --plotHeight 10 --plotWidth 10
+plotHeatmap -m Control_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz -out Control_3_30bControl_3_30b_heatmap.pdf
 
