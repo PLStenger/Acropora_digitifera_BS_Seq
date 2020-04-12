64d5c42251cca128c3a06a3358d17c8e200f3a19
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-04-13 10:15:03 +1100

    Update 16_04_compute_matrix.sh

--- 00_scripts/16_04_compute_matrix.sh
+++ 00_scripts/16_04_compute_matrix.sh
@@ -4,7 +4,9 @@ DATADIRECTORY=/home/stenger/stenger_data/EPIGENETIC_DATA/05_02_bismark
 
 cd $DATADIRECTORY
 
-computeMatrix scale-regions -S Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R scaffold_gene_lenght_02.bed  --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz
+#computeMatrix scale-regions -S Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R scaffold_gene_lenght_02.bed  --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz
 
-plotProfile -m Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz -out Control_3_30c.pdf --plotTitle "Control_3_30c" --plotHeight 10 --plotWidth 10
+#plotProfile -m Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz -out Control_3_30c.pdf --plotTitle "Control_3_30c" --plotHeight 10 --plotWidth 10
+
+plotHeatmap -m Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz -out Control_3_30c_heatmap.pdf 
 
