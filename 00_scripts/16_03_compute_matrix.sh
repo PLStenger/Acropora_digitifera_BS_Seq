b1ab1857287b6b769c494c01dfbcea7d3dcd8295
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-04-13 10:09:33 +1100

    Update 16_03_compute_matrix.sh

--- 00_scripts/16_03_compute_matrix.sh
+++ 00_scripts/16_03_compute_matrix.sh
@@ -6,5 +6,6 @@ cd $DATADIRECTORY
 
 computeMatrix scale-regions -S Acclimation_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R scaffold_gene_lenght_02.bed  --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o Acclimation_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz
 
-plotProfile -m Acclimation_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz -out Acclimation_3_30a.pdf --plotTitle "Acclimation_3_30a" --plotHeight 10 --plotWidth 10
+#plotProfile -m Acclimation_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz -out Acclimation_3_30a.pdf --plotTitle "Acclimation_3_30a" --plotHeight 10 --plotWidth 10
 
+plotHeatmap -m Acclimation_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz -out Acclimation_3_30a_heatmap.pdf
