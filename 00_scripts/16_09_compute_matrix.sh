494784e0c1a1387d5ffa81e648554659d60345d0
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-04-14 10:34:49 +1100

    Update 16_09_compute_matrix.sh

--- 00_scripts/16_09_compute_matrix.sh
+++ 00_scripts/16_09_compute_matrix.sh
@@ -4,7 +4,7 @@ DATADIRECTORY=/home/stenger/stenger_data/EPIGENETIC_DATA/05_02_bismark
 
 cd $DATADIRECTORY
 
-computeMatrix scale-regions -S Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R scaffold_gene_lenght_02.bed  --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz
+#computeMatrix scale-regions -S Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R scaffold_gene_lenght_02.bed  --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz
 
 #plotProfile -m Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz -out Control_1_30a.pdf --plotTitle "Control_1_30a" --plotHeight 10 --plotWidth 10
 plotHeatmap -m Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz -out Control_1_30a_heatmap.pdf
