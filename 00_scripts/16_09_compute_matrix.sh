6dc6ad5de31bf8e68ae561105076b0df40af97e8
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-06-12 17:20:29 +1100

    Update 16_09_compute_matrix.sh

--- 00_scripts/16_09_compute_matrix.sh
+++ 00_scripts/16_09_compute_matrix.sh
@@ -4,9 +4,96 @@ DATADIRECTORY=/home/stenger/stenger_data/EPIGENETIC_DATA/05_02_bismark
 
 cd $DATADIRECTORY
 
-#computeMatrix scale-regions -S Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R scaffold_gene_lenght_02.bed  --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz
+#computeMatrix scale-regions -S Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R scaffold_gene_lenght_02.bed  --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz
 
-#plotProfile -m Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz -out Control_1_30a.pdf --plotTitle "Control_1_30a" --plotHeight 10 --plotWidth 10
-plotHeatmap -m Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz -out Control_1_30a_heatmap.pdf
+#plotProfile -m Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out Acclimation_1_31_5b.pdf --plotTitle "Acclimation_1_31_5b" --plotHeight 10 --plotWidth 10
+
+#plotHeatmap -m Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out Acclimation_1_31_5b_heatmap.pdf 
+
+#######################################################################################################################################################################
+#######################################################################################################################################################################
+
+computeMatrix scale-regions -S Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R FC_A1vsA3_down.bed --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o FC_A1vsA3_down_Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz
+
+plotHeatmap -m FC_A1vsA3_down_Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_A1vsA3_down_Acclimation_1_31_5b_heatmap.pdf 
+
+#######################################################################################################################################################################
+
+computeMatrix scale-regions -S Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R FC_A1vsA3_up.bed --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o FC_A1vsA3_up_Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz
+
+plotHeatmap -m FC_A1vsA3_up_Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_A1vsA3_up_Acclimation_1_31_5b_heatmap.pdf 
+
+#######################################################################################################################################################################
+#######################################################################################################################################################################
+
+computeMatrix scale-regions -S Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R FC_A1vsC3_down.bed --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o FC_A1vsC3_down_Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz
+
+plotHeatmap -m FC_A1vsC3_down_Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_A1vsC3_down_Acclimation_1_31_5b_heatmap.pdf 
+
+#######################################################################################################################################################################
+
+computeMatrix scale-regions -S Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R FC_A1vsC3_up.bed --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o FC_A1vsC3_up_Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz
+
+plotHeatmap -m FC_A1vsC3_up_Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_A1vsC3_up_Acclimation_1_31_5b_heatmap.pdf 
+
+#######################################################################################################################################################################
+#######################################################################################################################################################################
+
+computeMatrix scale-regions -S Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R FC_C1vsA1_down.bed --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o FC_C1vsA1_down_Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz
+
+plotHeatmap -m FC_C1vsA1_down_Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_C1vsA1_down_Acclimation_1_31_5b_heatmap.pdf 
+
+#######################################################################################################################################################################
+
+computeMatrix scale-regions -S Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R FC_C1vsA1_up.bed --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o FC_C1vsA1_up_Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz
+
+plotHeatmap -m FC_C1vsA1_up_Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_C1vsA1_up_Acclimation_1_31_5b_heatmap.pdf 
+
+#######################################################################################################################################################################
+#######################################################################################################################################################################
+
+computeMatrix scale-regions -S Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R FC_C1vsA3_down.bed --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o FC_C1vsA3_down_Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz
+
+plotHeatmap -m FC_C1vsA3_down_Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_C1vsA3_down_Acclimation_1_31_5b_heatmap.pdf 
+
+#######################################################################################################################################################################
+
+computeMatrix scale-regions -S Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R FC_C1vsA3_up.bed --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o FC_C1vsA3_up_Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz
+
+plotHeatmap -m FC_C1vsA3_up_Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_C1vsA3_up_Acclimation_1_31_5b_heatmap.pdf 
+
+#######################################################################################################################################################################
+#######################################################################################################################################################################
+
+computeMatrix scale-regions -S Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R FC_C1vsC3_down.bed --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o FC_C1vsC3_down_Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz
+
+plotHeatmap -m FC_C1vsC3_down_Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_C1vsC3_down_Acclimation_1_31_5b_heatmap.pdf 
+
+#######################################################################################################################################################################
+
+computeMatrix scale-regions -S Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R FC_C1vsC3_up.bed --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o FC_C1vsC3_up_Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz
+
+plotHeatmap -m FC_C1vsC3_up_Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_C1vsC3_up_Acclimation_1_31_5b_heatmap.pdf 
+
+#######################################################################################################################################################################
+#######################################################################################################################################################################
+
+computeMatrix scale-regions -S Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R FC_C3vsA3_down.bed --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o FC_C3vsA3_down_Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz
+
+plotHeatmap -m FC_C3vsA3_down_Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_C3vsA3_down_Acclimation_1_31_5b_heatmap.pdf 
+
+#######################################################################################################################################################################
+
+computeMatrix scale-regions -S Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw -R FC_C3vsA3_up.bed --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o FC_C3vsA3_up_Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz
+
+plotHeatmap -m FC_C3vsA3_up_Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz_matrix.mat.gz -out FC_C3vsA3_up_Acclimation_1_31_5b_heatmap.pdf 
+
+#######################################################################################################################################################################
+#######################################################################################################################################################################
+
+
+
+
+
 
 
