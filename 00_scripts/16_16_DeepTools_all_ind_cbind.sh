e7d4d93299450985e68a5c2f370e819c14694283
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-07-21 17:24:44 +1100

    Update 16_16_DeepTools_all_ind_cbind.sh

--- 00_scripts/16_16_DeepTools_all_ind_cbind.sh
+++ 00_scripts/16_16_DeepTools_all_ind_cbind.sh
@@ -4,14 +4,19 @@ DATADIRECTORY=/home/stenger/stenger_data/EPIGENETIC_DATA/05_02_bismark
 
 cd $DATADIRECTORY
 
-#computeMatrixOperations cbind -m Acclimation_1_31_5a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Acclimation_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Acclimation_1_31_5b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Acclimation_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_1_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Acclimation_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_1_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz \
-computeMatrixOperations cbind -m Acclimation_1_31_5a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Acclimation_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz -o all_individuals_cbind_matrix.mat.gz
+for FILE in $(ls $DATADIRECTORY/*.bam_sorted.bam.bw)
+do
+
+computeMatrix scale-regions -S ${FILE##*/} -R real_gene_lenght_32469_all_exons_introns_02.bed --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o ${FILE##*/}_matrix.mat.gz
+
+done ;
+
+computeMatrixOperations cbind -m Acclimation_1_31_5a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Acclimation_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Acclimation_1_31_5b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Acclimation_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_1_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Acclimation_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_1_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz \
  
-plotHeatmap -m all_individuals_cbind_matrix.mat.gz \
-     -out all_individuals_cbind_matrix.mat.gz.pdf 
+plotHeatmap -m all_individuals_cbind_matrix.mat.gz -out all_individuals_cbind_matrix.mat.gz.pdf 
 
 plotHeatmap -m all_individuals_cbind_matrix.mat.gz \
-     -out all_individuals_cbind_matrix.mat.gz_kmeans_04.pdf \
+     -out all_individuals_cbind_matrix.mat.gz_kmeans_02.pdf \
      --colorMap RdBu \
      --whatToShow 'plot, heatmap and colorbar' \
      --zMin -3 --zMax 3 \
@@ -19,12 +24,12 @@ plotHeatmap -m all_individuals_cbind_matrix.mat.gz \
      --outFileSortedRegions all_individuals_cbind_matrix.mat.gz_kmeans_02.bed
 
 plotHeatmap -m all_individuals_cbind_matrix.mat.gz \
-     -out all_individuals_cbind_matrix.mat.gz_kmeans_04.pdf \
+     -out all_individuals_cbind_matrix.mat.gz_kmeans_03.pdf \
      --colorMap RdBu \
      --whatToShow 'plot, heatmap and colorbar' \
      --zMin -3 --zMax 3 \
      --kmeans 3 \
-     --outFileSortedRegions all_individuals_cbind_matrix.mat.gz_kmeans_043.bed
+     --outFileSortedRegions all_individuals_cbind_matrix.mat.gz_kmeans_03.bed
 
 plotHeatmap -m all_individuals_cbind_matrix.mat.gz \
      -out all_individuals_cbind_matrix.mat.gz_kmeans_04.pdf \
