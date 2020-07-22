99a90790b9972d8d0580e3772541758566c14758
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-07-22 16:01:22 +1100

    Update 16_16_DeepTools_all_ind_cbind.sh

--- 00_scripts/16_16_DeepTools_all_ind_cbind.sh
+++ 00_scripts/16_16_DeepTools_all_ind_cbind.sh
@@ -11,30 +11,38 @@ cd $DATADIRECTORY
 
 #done ;
 
-computeMatrixOperations cbind -m Acclimation_1_31_5a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Acclimation_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Acclimation_1_31_5b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Acclimation_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_1_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Acclimation_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_1_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz -o all_individuals_cbind_matrix.mat.gz
- 
-plotHeatmap -m all_individuals_cbind_matrix.mat.gz -out all_individuals_cbind_matrix.mat.gz.pdf 
+for FILE in $(ls $DATADIRECTORY/*.bam_sorted.bam.bw)
+do
 
-plotHeatmap -m all_individuals_cbind_matrix.mat.gz \
-     -out all_individuals_cbind_matrix.mat.gz_kmeans_02.pdf \
+computeMatrix scale-regions -S ${FILE##*/} -R all_individuals_cbind_matrix.mat.gz_kmeans_04_for_input_without_clust1.bed --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o ${FILE##*/}_without_clust1_matrix.mat.gz
+
+done ;
+
+computeMatrixOperations cbind -m Acclimation_1_31_5a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_without_clust1_matrix.mat.gz Acclimation_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_without_clust1_matrix.mat.gz Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_without_clust1_matrix.mat.gz Control_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_without_clust1_matrix.mat.gz Acclimation_1_31_5b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_without_clust1_matrix.mat.gz Acclimation_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_without_clust1_matrix.mat.gz Control_1_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_without_clust1_matrix.mat.gz Control_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_without_clust1_matrix.mat.gz Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_without_clust1_matrix.mat.gz Acclimation_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_without_clust1_matrix.mat.gz Control_1_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_without_clust1_matrix.mat.gz Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_without_clust1_matrix.mat.gz -o all_individuals_cbind_without_clust1_matrix.mat.gz
+ 
+plotHeatmap -m all_individuals_cbind_without_clust1_matrix.mat.gz -out all_individuals_cbind_without_clust1_matrix.mat.gz.pdf 
+
+plotHeatmap -m all_individuals_cbind_without_clust1_matrix.mat.gz \
+     -out all_individuals_cbind_without_clust1_matrix.mat.gz_kmeans_02.pdf \
      --colorMap RdBu \
      --whatToShow 'plot, heatmap and colorbar' \
      --zMin -3 --zMax 3 \
      --kmeans 2 \
-     --outFileSortedRegions all_individuals_cbind_matrix.mat.gz_kmeans_02.bed
+     --outFileSortedRegions all_individuals_cbind_without_clust1_matrix.mat.gz_kmeans_02.bed
 
-plotHeatmap -m all_individuals_cbind_matrix.mat.gz \
-     -out all_individuals_cbind_matrix.mat.gz_kmeans_03.pdf \
+plotHeatmap -m all_individuals_cbind_without_clust1_matrix.mat.gz \
+     -out all_individuals_cbind_without_clust1_matrix.mat.gz_kmeans_03.pdf \
      --colorMap RdBu \
      --whatToShow 'plot, heatmap and colorbar' \
      --zMin -3 --zMax 3 \
      --kmeans 3 \
-     --outFileSortedRegions all_individuals_cbind_matrix.mat.gz_kmeans_03.bed
+     --outFileSortedRegions all_individuals_cbind_without_clust1_matrix.mat.gz_kmeans_03.bed
 
-plotHeatmap -m all_individuals_cbind_matrix.mat.gz \
-     -out all_individuals_cbind_matrix.mat.gz_kmeans_04.pdf \
+plotHeatmap -m all_individuals_cbind_without_clust1_matrix.mat.gz \
+     -out all_individuals_cbind_without_clust1_matrix.mat.gz_kmeans_04.pdf \
      --colorMap RdBu \
      --whatToShow 'plot, heatmap and colorbar' \
      --zMin -3 --zMax 3 \
      --kmeans 4 \
-     --outFileSortedRegions all_individuals_cbind_matrix.mat.gz_kmeans_04.bed
+     --outFileSortedRegions all_individuals_cbind_without_clust1_matrix.mat.gz_kmeans_04.bed
+
