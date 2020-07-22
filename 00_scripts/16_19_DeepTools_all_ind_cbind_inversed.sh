8a1d21e85cf28b7d489fbd1234c6a59010c90b31
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-07-22 15:12:06 +1100

    Update 16_19_DeepTools_all_ind_cbind_inversed.sh

--- 00_scripts/16_19_DeepTools_all_ind_cbind_inversed.sh
+++ 00_scripts/16_19_DeepTools_all_ind_cbind_inversed.sh
@@ -11,40 +11,52 @@ cd $DATADIRECTORY
 
 #done ;
 
-computeMatrixOperations rbind -m Acclimation_1_31_5a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz -o Acclimation_1_rbind_inversed_matrix.mat.gz
+#computeMatrixOperations rbind -m Acclimation_1_31_5a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_1_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Acclimation_1_31_5c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz -o Acclimation_1_rbind_inversed_matrix.mat.gz
 
-computeMatrixOperations rbind -m Acclimation_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Acclimation_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz -o Acclimation_3_rbind_inversed_matrix.mat.gz
+#computeMatrixOperations rbind -m Acclimation_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Acclimation_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz -o Acclimation_3_rbind_inversed_matrix.mat.gz
 
-computeMatrixOperations rbind -m Acclimation_1_31_5b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_1_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz -o Control_1_rbind_inversed_matrix.mat.gz
+#computeMatrixOperations rbind -m Acclimation_1_31_5b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_1_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz -o Control_1_rbind_inversed_matrix.mat.gz
 
-computeMatrixOperations rbind -m Acclimation_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_1_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz -o Control_3_rbind_inversed_matrix.mat.gz
+#computeMatrixOperations rbind -m Acclimation_3_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_1_30b_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz -o Control_3_rbind_inversed_matrix.mat.gz
 
-computeMatrixOperations cbind -m Acclimation_1_rbind_inversed_matrix.mat.gz Acclimation_3_rbind_inversed_matrix.mat.gz Control_1_rbind_inversed_matrix.mat.gz Control_3_rbind_inversed_matrix.mat.gz -o all_genes_cbind_inversed_matrix.mat.gz
+#computeMatrixOperations cbind -m Acclimation_1_rbind_inversed_matrix.mat.gz Acclimation_3_rbind_inversed_matrix.mat.gz Control_1_rbind_inversed_matrix.mat.gz Control_3_rbind_inversed_matrix.mat.gz -o all_genes_cbind_inversed_matrix.mat.gz
 
+computeMatrixOperations rbind -m Acclimation_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Control_3_30a_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz Acclimation_3_30c_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.bw_matrix.mat.gz -o Control_3_rbind_inversed_matrix.mat.gz
 
  
-plotHeatmap -m all_genes_cbind_inversed_matrix.mat.gz -out all_genes_cbind_inversed_matrix.mat.gz.pdf 
+#plotHeatmap -m all_genes_cbind_inversed_matrix.mat.gz -out all_genes_cbind_inversed_matrix.mat.gz.pdf 
+#
+#plotHeatmap -m all_genes_cbind_inversed_matrix.mat.gz \
+#     -out all_genes_cbind_inversed_matrix.mat.gz_kmeans_02.pdf \
+#     --colorMap RdBu \
+#     --whatToShow 'plot, heatmap and colorbar' \
+#     --zMin -3 --zMax 3 \
+#     --kmeans 2 \
+#     --outFileSortedRegions all_genes_cbind_inversed_matrix.mat.gz_kmeans_02.bed
+#
+#plotHeatmap -m all_genes_cbind_inversed_matrix.mat.gz \
+#     -out all_genes_cbind_inversed_matrix.mat.gz_kmeans_03.pdf \
+#     --colorMap RdBu \
+#     --whatToShow 'plot, heatmap and colorbar' \
+#     --zMin -3 --zMax 3 \
+#     --kmeans 3 \
+#     --outFileSortedRegions all_genes_cbind_inversed_matrix.mat.gz_kmeans_03.bed
+#
+#plotHeatmap -m all_genes_cbind_inversed_matrix.mat.gz \
+#     -out all_genes_cbind_inversed_matrix.mat.gz_kmeans_04.pdf \
+#     --colorMap RdBu \
+#     --whatToShow 'plot, heatmap and colorbar' \
+#     --zMin -3 --zMax 3 \
+#     --kmeans 4 \
+#     --outFileSortedRegions all_genes_cbind_inversed_matrix.mat.gz_kmeans_04.bed
+#     
+     
 
-plotHeatmap -m all_genes_cbind_inversed_matrix.mat.gz \
-     -out all_genes_cbind_inversed_matrix.mat.gz_kmeans_02.pdf \
-     --colorMap RdBu \
-     --whatToShow 'plot, heatmap and colorbar' \
-     --zMin -3 --zMax 3 \
-     --kmeans 2 \
-     --outFileSortedRegions all_genes_cbind_inversed_matrix.mat.gz_kmeans_02.bed
-
-plotHeatmap -m all_genes_cbind_inversed_matrix.mat.gz \
-     -out all_genes_cbind_inversed_matrix.mat.gz_kmeans_03.pdf \
-     --colorMap RdBu \
-     --whatToShow 'plot, heatmap and colorbar' \
-     --zMin -3 --zMax 3 \
-     --kmeans 3 \
-     --outFileSortedRegions all_genes_cbind_inversed_matrix.mat.gz_kmeans_03.bed
-
-plotHeatmap -m all_genes_cbind_inversed_matrix.mat.gz \
-     -out all_genes_cbind_inversed_matrix.mat.gz_kmeans_04.pdf \
+plotHeatmap -m Control_3_rbind_inversed_matrix.mat.gz \
+     -out Control_3_rbind_inversed_matrix.mat.gz_kmeans_04.pdf \
      --colorMap RdBu \
      --whatToShow 'plot, heatmap and colorbar' \
      --zMin -3 --zMax 3 \
      --kmeans 4 \
-     --outFileSortedRegions all_genes_cbind_inversed_matrix.mat.gz_kmeans_04.bed
+     --outFileSortedRegions Control_3_rbind_inversed_matrix.mat.gz_kmeans_04.bed
+     
