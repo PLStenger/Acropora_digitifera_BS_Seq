e69af21e84f8074d92a468ce0cc9b417af8378c7
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-08-26 11:40:00 +1100

    Update 21_comparative_hclust.R

--- 00_scripts/21_comparative_hclust.R
+++ 00_scripts/21_comparative_hclust.R
@@ -33,6 +33,13 @@ colnames(dat2) <- c("Acclimation_1_31_5a", "Acclimation_1_31_5b", "Acclimation_1
 head(dat2)
 dat3 <- t(dat2)
 
+library(factoextra)
+fviz_nbclust(dat3, FUN = hcut, method = "wss")
+fviz_nbclust(dat3, FUN = hcut, method = "silhouette")
+library(cluster)
+gap_stat <- clusGap(dat3, FUN = hcut, nstart = 25, K.max = 10, B = 50)
+fviz_gap_stat(gap_stat)
+
 # Euclidean distance
 dist <- dist(dat3 , diag=TRUE)
 
@@ -48,13 +55,46 @@ plot(hc)
 
 
 # Create two dendrograms
-dend1 <- as.dendrogram (hc)
-dend2 <- as.dendrogram (hc2)
+d1 <- as.dendrogram (hc)
+d2 <- as.dendrogram (hc2)
 
 # Create a list to hold dendrograms
-dend_list <- dendlist(dend1, dend2)
+dend_list <- dendlist(d1, d2)
+
+pdf(file= 'Comparative_dendrogram.pdf' ,onefile=T,paper='A4')
+
 
 # Align and plot two dendrograms side by side
-dendlist(dend1, dend2) %>%
+dendlist(d1, d2) %>%
   untangle(method = "step1side") %>% # Find the best alignment layout
   tanglegram()                       # Draw the two dendrograms
+
+dev.off()
+
+
+
+
+
+
+pdf(file= 'Comparative_dendrogram_02.pdf' ,onefile=T,paper='A4')
+
+# Custom these kendo, and place them in a list
+dl <- dendlist(
+  d1 %>% 
+    set("labels_col", value = c("skyblue", "orange", "grey"), k=3) %>%
+    set("branches_lty", 1) %>%
+    set("branches_k_color", value = c("skyblue", "orange", "grey"), k = 3),
+  d2 %>% 
+    set("labels_col", value = c("skyblue", "orange", "grey"), k=3) %>%
+    set("branches_lty", 1) %>%
+    set("branches_k_color", value = c("skyblue", "orange", "grey"), k = 3)
+)
+
+# Plot them together
+tanglegram(dl, 
+           common_subtrees_color_lines = FALSE, highlight_distinct_edges  = TRUE, highlight_branches_lwd=FALSE, 
+           margin_inner=7,
+           lwd=2
+)
+
+dev.off()
