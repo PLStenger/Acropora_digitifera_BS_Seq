26d291bf6238ec77dfb7a01c68aea50d2bfef295
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-10-19 15:38:22 +1100

    Update 19_05_transpose_for_large_file.sh

--- 00_scripts/19_05_transpose_for_large_file.sh
+++ 00_scripts/19_05_transpose_for_large_file.sh
@@ -4,6 +4,12 @@ DATADIRECTORY=/home/stenger/stenger_data/99_software/angsd
 
 cd $DATADIRECTORY
 
-rs -c' ' -C' ' -T outfile.geno_02_good_column_nb_header_newline.txt > outfile.geno_02_good_column_nb_header_newline_transpose.txt
+awk '{for (i=1; i<=NF; i++) a[i,NR]=$i
+        max=(max<NF?NF:max)}
+        END {for (i=1; i<=max; i++)
+              {for (j=1; j<=NR; j++) 
+                  printf "%s%s", a[i,j], (j==NR?RS:FS)
+              }
+        }'  outfile.geno_02_good_column_nb_header_newline.txt > outfile.geno_02_good_column_nb_header_newline_transpose.txt
 
 
